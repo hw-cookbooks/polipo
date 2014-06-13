ebf8448b3564a5cf9e6969af2da110a1214e066c
Chris Roberts <chrisroberts.code@gmail.com>
2014-06-13 06:07:40 -0700

    Add default log configuration. Include service user within attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,11 @@ default[:polipo][:install][:version] = nil
 default[:polipo][:directories][:config] = '/etc/polipo'
 
 default[:polipo][:config][:allowed_clients] = '127.0.0.1'
+default[:polipo][:config][:log_syslog] = false
+default[:polipo][:config][:log_file] = '/var/log/polipo/polipo.log'
 
 default[:polipo][:options][:method] = ['any']
 
 default[:polipo][:forbidden] = []
+
+default[:polipo][:service][:user] = 'proxy'
